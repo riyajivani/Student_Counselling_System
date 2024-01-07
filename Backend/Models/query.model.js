6f439d3ffee93e6f0a128baed31bb3480834de07
monilghori <monilghori46@gmail.com>
2024-01-07 11:53:51 +0530

    add mode field

--- Backend/Models/query.model.js
+++ Backend/Models/query.model.js
@@ -6,6 +6,7 @@ const queryschema = new mongoose.Schema({
         querybystudent : {type : mongoose.Schema.Types.ObjectId, ref : "student"},
         solvebyfaculty : {type : mongoose.Schema.Types.ObjectId, ref : "faculty"},
         solution : {type : String},
+        mode : {type : String , default : "private"},
         status : {type : String , default : "Not solved"}
 },
 {
