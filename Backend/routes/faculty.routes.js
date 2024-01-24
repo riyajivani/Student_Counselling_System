3cdff05be7b5ed7c9bf183e2ab641586b17c6183
monilghori <monilghori46@gmail.com>
2024-01-25 00:28:14 +0530

    change post to put and delete.

--- Backend/routes/faculty.routes.js
+++ Backend/routes/faculty.routes.js
@@ -4,7 +4,7 @@ const facultyController = require("../Controllers/faculty.controller")
 const { validate4signup ,validate4login } = require("../utils/joi.validate")
 
 
-router.post("/createfaculty",validate4signup,facultyController.createFaculty)
+router.put("/createfaculty",validate4signup,facultyController.createFaculty)
 router.post("/login",validate4login,facultyController.facultyLogin)
 router.get("/getstudents",facultyController.getStudentsByBatch)
 
--- Backend/routes/student.routes.js
+++ Backend/routes/student.routes.js
@@ -5,7 +5,7 @@ const queryController = require("../Controllers/query.controller")
 const { validate4signup ,validate4login ,validate4askQuery} = require("../utils/joi.validate")
 
 
-router.post("/createstudent",validate4signup,studentController.createstudent)
+router.put("/createstudent",validate4signup,studentController.createstudent)
 router.post("/login",validate4login,studentController.studentLogin)
 router.post("/askmentor",validate4askQuery,queryController.askMentor)
 
