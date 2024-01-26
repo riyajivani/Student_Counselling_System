f6998e5f9c282c282e686c96ba8510b8aff6af41
monilghori <monilghori46@gmail.com>
2024-01-27 00:35:20 +0530

    add fuculty id in query model.

--- Backend/Models/query.model.js
+++ Backend/Models/query.model.js
@@ -4,6 +4,7 @@ const queryschema = new mongoose.Schema({
         query: {type : String},
         querybystudent : {type : mongoose.Schema.Types.ObjectId, ref : "student"},
         solvebyfaculty : {type : mongoose.Schema.Types.ObjectId, ref : "faculty"},
+        facultyId : {type : mongoose.Schema.Types.ObjectId, ref : "faculty"},
         solution : {type : String},
         mode : {type : String , default : "private"},
         status : {type : String , default : "Not solved"}
