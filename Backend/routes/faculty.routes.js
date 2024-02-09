eb0af3b829b351ddd614c5fc37e4290f53f9f5cb
Riya Jivani <riyajivani8@gmail.com>
2024-02-09 15:53:53 +0530

    faculty pages are being developed

--- Backend/routes/faculty.routes.js
+++ Backend/routes/faculty.routes.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const router = express.Router()
 const facultyController = require("../Controllers/faculty.controller")
-const { validate4signup ,validate4login } = require("../utils/joi.validate")
+const { validate4signup, validate4login } = require("../utils/joi.validate")
 const queryController = require("../Controllers/query.controller")
 
 
 router.put("/createfaculty", validate4signup, facultyController.createFaculty)
 router.post("/login", validate4login, facultyController.facultyLogin)
 router.get("/getstudents", facultyController.getStudentsByBatch)
-router.get("/getquery", queryController.displayQueryToFaculty)
+router.post("/getquery", queryController.displayQueryToFaculty)
 router.get("/publicquery", queryController.publicQuery)
 router.get("/querybystatus", queryController.queryByStatusForFaculty)
 
