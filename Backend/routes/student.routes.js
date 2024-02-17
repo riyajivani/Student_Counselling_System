172810b2cbef8e03ac38335cf835fc4868f753af
monilghori <monilghori46@gmail.com>
2024-02-18 00:15:15 +0530

    chnage name of function of controller(display).

--- Backend/routes/student.routes.js
+++ Backend/routes/student.routes.js
@@ -2,16 +2,15 @@ const express = require("express")
 const router = express.Router()
 const studentController = require("../Controllers/student.controller")
 const queryController = require("../Controllers/query.controller")
-const { validate4signup ,validate4login ,validate4askQuery, validate4changeMode} = require("../utils/joi.validate")
+const { validate4signup, validate4login, validate4askQuery, validate4changeMode } = require("../utils/joi.validate")
 
 
-router.put("/createstudent",validate4signup,studentController.createstudent)
-router.post("/login",validate4login,studentController.studentLogin)
-router.post("/askmentor",validate4askQuery,queryController.askMentor)
-router.put("/changemode",validate4changeMode,queryController.changeMode)
-router.get("/publicquery",queryController.publicQuery)
-router.post("/getquery",queryController.queryBySatusForStudent)
-
+router.put("/createstudent", validate4signup, studentController.createstudent)
+router.post("/login", validate4login, studentController.studentLogin)
+router.post("/askmentor", validate4askQuery, queryController.askMentor)
+router.put("/changemode", validate4changeMode, queryController.changeMode)
+router.get("/publicquery", queryController.publicQuery)
+router.post("/getquery",queryController.displayQueryBySatusForStudent)
 
 
 module.exports = router
